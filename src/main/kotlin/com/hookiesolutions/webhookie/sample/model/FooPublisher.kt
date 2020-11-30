7aec2862a7f584c26b107842a2982b6240ac573a
Arthur Kazemi <bidadh@gmail.com>
2020-11-30 18:03:49 +1100

    refactor

--- src/main/kotlin/com/hookiesolutions/webhookie/sample/model/FooPublisher.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/sample/model/FooPublisher.kt
@@ -4,7 +4,6 @@ import com.hookiesolutions.webhookie.model.AbstractEntity
 import org.springframework.data.annotation.TypeAlias
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.http.MediaType
 
 /**
  *
