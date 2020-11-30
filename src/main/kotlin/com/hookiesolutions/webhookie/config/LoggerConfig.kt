5f387a54bd48f7e470bbf3e8d36b77de73450fb4
Arthur Kazemi <bidadh@gmail.com>
2020-11-30 13:12:43 +1100

    add Logger bean

---
+++ src/main/kotlin/com/hookiesolutions/webhookie/config/LoggerConfig.kt
@@ -0,0 +1,24 @@
+package com.hookiesolutions.webhookie.config
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.InjectionPoint
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Scope
+
+/**
+ *
+ * @author Arthur Kazemi<bidadh@gmail.com>
+ * @since 2/12/19 01:32
+ */
+@Configuration
+class LoggerConfig {
+  @Bean
+  @Scope("prototype")
+  fun logger(injectionPoint: InjectionPoint): Logger {
+    return LoggerFactory.getLogger(
+        injectionPoint.methodParameter?.containingClass // constructor
+            ?: injectionPoint.field?.declaringClass) // or field injection
+  }
+}
