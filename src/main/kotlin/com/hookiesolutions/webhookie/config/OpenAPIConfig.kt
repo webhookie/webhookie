37bf23f5f0373b98d7cf398238f5fd498dc4d769
Arthur Kazemi <bidadh@gmail.com>
2020-12-03 22:22:47 +1100

    add overall OpenAPIConfig

---
+++ src/main/kotlin/com/hookiesolutions/webhookie/config/OpenAPIConfig.kt
@@ -0,0 +1,30 @@
+package com.hookiesolutions.webhookie.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import org.springframework.context.annotation.Configuration
+
+/**
+ *
+ * @author Arthur Kazemi<bidadh@gmail.com>
+ * @since 3/12/20 17:33
+ */
+@Configuration
+@OpenAPIDefinition(
+    info = Info(
+        title = "webHookie API",
+        version = "1.0.0"
+    )
+)
+@SecurityScheme(
+    name = OpenAPIConfig.BASIC_SCHEME,
+    type = SecuritySchemeType.HTTP,
+    scheme = "basic"
+)
+class OpenAPIConfig {
+  companion object {
+    const val BASIC_SCHEME = "basicScheme"
+  }
+}
