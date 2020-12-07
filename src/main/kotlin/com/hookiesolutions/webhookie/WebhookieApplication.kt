6d3401b0fd2a5fa4751dc5c4b59b1e9168e960f7
Arthur Kazemi <bidadh@gmail.com>
2020-12-07 17:09:16 +1100

    enable Caching

--- src/main/kotlin/com/hookiesolutions/webhookie/WebhookieApplication.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/WebhookieApplication.kt
@@ -2,9 +2,11 @@ package com.hookiesolutions.webhookie
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import reactor.tools.agent.ReactorDebugAgent
 
 @SpringBootApplication
+@EnableCaching
 class WebhookieApplication
 
 fun main(args: Array<String>) {
