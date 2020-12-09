e3ba7ef513c32f16980000a9d89adc0249f3617e
Arthur Kazemi <bidadh@gmail.com>
2020-12-09 19:48:22 +1100

    minor change

--- src/main/kotlin/com/hookiesolutions/webhookie/config/web/OpenAPIConfig.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/config/web/OpenAPIConfig.kt
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @OpenAPIDefinition(
     info = Info(
-        title = "webHookie API",
+        title = "webhookie API",
         version = "1.0.0"
     )
 )
