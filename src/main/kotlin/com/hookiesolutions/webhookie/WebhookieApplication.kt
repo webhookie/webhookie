cbe1a20508a173e33e1a8e5fdbcb3f7789494e40
Arthur Kazemi <bidadh@gmail.com>
2020-12-09 15:44:08 +1100

    minor change

--- src/main/kotlin/com/hookiesolutions/webhookie/WebhookieApplication.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/WebhookieApplication.kt
@@ -3,10 +3,16 @@ package com.hookiesolutions.webhookie
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.integration.config.EnableIntegration
+import org.springframework.integration.config.EnableIntegrationManagement
+import org.springframework.integration.http.config.EnableIntegrationGraphController
 import reactor.tools.agent.ReactorDebugAgent
 
 @SpringBootApplication
 @EnableCaching
+@EnableIntegration
+@EnableIntegrationManagement
+@EnableIntegrationGraphController
 class WebhookieApplication
 
 fun main(args: Array<String>) {
