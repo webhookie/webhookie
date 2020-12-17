33b868ad38d08b078bf660b524e4c1409ecece55
Arthur Kazemi <bidadh@gmail.com>
2020-12-17 13:21:11 +1100

    minor change

--- src/main/kotlin/com/hookiesolutions/webhookie/subscription/domain/BlockedSubscriptionMessage.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/subscription/domain/BlockedSubscriptionMessage.kt
@@ -85,4 +85,28 @@ data class BlockedSubscriptionMessage(
       )
     }
   }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is BlockedSubscriptionMessage) return false
+
+    if (headers != other.headers) return false
+    if (originalSpanId != other.originalSpanId) return false
+    if (!payload.contentEquals(other.payload)) return false
+    if (messageHeaders != other.messageHeaders) return false
+    if (subscription != other.subscription) return false
+    if (blockedDetails != other.blockedDetails) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = headers.hashCode()
+    result = 31 * result + originalSpanId.hashCode()
+    result = 31 * result + payload.contentHashCode()
+    result = 31 * result + messageHeaders.hashCode()
+    result = 31 * result + subscription.hashCode()
+    result = 31 * result + blockedDetails.hashCode()
+    return result
+  }
 }
