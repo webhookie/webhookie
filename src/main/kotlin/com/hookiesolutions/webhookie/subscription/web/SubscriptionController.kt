1736ab979f3c461f9a5ba5592a563780e15f7259
Arthur Kazemi <bidadh@gmail.com>
2021-01-01 11:52:49 +1100

    block subscription API

--- src/main/kotlin/com/hookiesolutions/webhookie/subscription/web/SubscriptionController.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/subscription/web/SubscriptionController.kt
@@ -1,13 +1,19 @@
 package com.hookiesolutions.webhookie.subscription.web
 
+import com.hookiesolutions.webhookie.common.model.AbstractEntity.Queries.Companion.byId
+import com.hookiesolutions.webhookie.common.model.dto.BlockedDetailsDTO
 import com.hookiesolutions.webhookie.common.model.dto.SubscriptionDTO
+import com.hookiesolutions.webhookie.common.service.TimeMachine
 import com.hookiesolutions.webhookie.config.web.OpenAPIConfig
+import com.hookiesolutions.webhookie.subscription.domain.Subscription
 import com.hookiesolutions.webhookie.subscription.service.SubscriptionService
 import com.hookiesolutions.webhookie.subscription.service.model.CreateSubscriptionRequest
 import com.hookiesolutions.webhookie.subscription.web.ApplicationController.Companion.REQUEST_MAPPING_APPLICATION
 import com.hookiesolutions.webhookie.subscription.web.CompanyController.Companion.REQUEST_MAPPING_COMPANY
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.slf4j.Logger
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -25,6 +31,8 @@ import javax.validation.Valid
 @RestController
 @SecurityRequirement(name = OpenAPIConfig.BASIC_SCHEME)
 class SubscriptionController(
+  private val timeMachine: TimeMachine,
+  private val mongoTemplate: ReactiveMongoTemplate,
   private val log: Logger,
   private val subscriptionService: SubscriptionService,
 ) {
@@ -36,6 +44,25 @@ class SubscriptionController(
     log.info("Unblocking subscription: '{}'", id)
     return subscriptionService.unblockSubscriptionBy(id)
       .map { it.id!! }
+  }
+
+  @PatchMapping(
+    "$REQUEST_MAPPING_COMPANY$REQUEST_MAPPING_SUBSCRIPTION/{id}/block",
+    produces = [MediaType.TEXT_PLAIN_VALUE]
+  )
+  fun blockSubscription(@PathVariable id: String): Mono<String> {
+    log.info("Unblocking subscription: '{}'", id)
+    val details = BlockedDetailsDTO("my Reason", timeMachine.now())
+    return mongoTemplate
+      .updateFirst(
+        Query.query(byId(id)),
+        Subscription.Updates.blockSubscription(details),
+        Subscription::class.java
+      )
+      .doOnNext {
+        log.info("Subscription({}) was blocked because '{}'", id, details.reason)
+      }
+      .map { details.reason }
   }
 
   @PostMapping(
