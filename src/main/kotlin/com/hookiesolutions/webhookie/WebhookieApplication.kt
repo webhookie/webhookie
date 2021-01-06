8eb95d03d0dd3ce163bb348456779d5221fcff4d
Arthur Kazemi <bidadh@gmail.com>
2021-01-06 16:21:23 +1100

    fix debug agent issue running as docker

--- src/main/kotlin/com/hookiesolutions/webhookie/WebhookieApplication.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/WebhookieApplication.kt
@@ -1,21 +1,31 @@
 package com.hookiesolutions.webhookie
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.context.event.EventListener
 import org.springframework.integration.config.EnableIntegration
 import org.springframework.integration.config.EnableIntegrationManagement
-import org.springframework.integration.http.config.EnableIntegrationGraphController
 import reactor.tools.agent.ReactorDebugAgent
 
 @SpringBootApplication
 @EnableCaching
 @EnableIntegration
 @EnableIntegrationManagement
-@EnableIntegrationGraphController
 class WebhookieApplication
 
 fun main(args: Array<String>) {
-	ReactorDebugAgent.init()
 	runApplication<WebhookieApplication>(*args)
+}
+
+@Configuration
+@Profile("dev")
+class DebugAgentConfig {
+	@EventListener(ApplicationReadyEvent::class)
+	fun initForDebug() {
+		ReactorDebugAgent.init()
+	}
 }
