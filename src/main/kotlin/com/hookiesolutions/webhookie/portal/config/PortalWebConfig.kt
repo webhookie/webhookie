cc14341488b02f42a8011bce4efb5f01224b3ba5
Arthur Kazemi <bidadh@gmail.com>
2021-01-12 15:40:56 +1100

    refactor

--- src/main/kotlin/com/hookiesolutions/webhookie/portal/config/PortalWebConfig.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/portal/config/PortalWebConfig.kt
@@ -1,5 +1,8 @@
 package com.hookiesolutions.webhookie.portal.config
 
+import amf.ProfileName
+import amf.client.parse.Async20Parser
+import amf.client.parse.Parser
 import com.hookiesolutions.webhookie.portal.io.AsyncDocumentEncoder
 import com.hookiesolutions.webhookie.portal.io.AsyncDocumentReader
 import org.springframework.boot.web.codec.CodecCustomizer
@@ -22,5 +25,15 @@ class PortalWebConfig {
       it.customCodecs().register(reader)
       it.customCodecs().register(encoder)
     }
+  }
+
+  @Bean
+  fun async20Parser(): Parser {
+    return Async20Parser()
+  }
+
+  @Bean
+  fun asyncProfileName(): ProfileName {
+    return ProfileName.apply("Async20Profile")
   }
 }
--- src/main/kotlin/com/hookiesolutions/webhookie/portal/io/AsyncDocumentReader.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/portal/io/AsyncDocumentReader.kt
@@ -1,19 +1,15 @@
 package com.hookiesolutions.webhookie.portal.io
 
-import amf.ProfileName
-import amf.client.AMF
 import amf.client.model.document.Document
-import amf.client.parse.Async20Parser
-import amf.client.validate.ValidationReport
 import org.springframework.core.ResolvableType
-import org.springframework.core.io.buffer.DataBuffer
-import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.MediaType
 import org.springframework.http.ReactiveHttpInputMessage
 import org.springframework.http.codec.HttpMessageReader
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
 import java.nio.charset.Charset
 
 @Component
@@ -39,8 +35,8 @@ class AsyncDocumentReader(
   ): Flux<Document> {
     val charset: Charset = message.headers.contentType?.charset ?: Charset.defaultCharset()
 
-    return DataBufferUtils.join(message.body)
-      .flatMapMany { reader.bufferToDocument(it, charset) }
+    return reader.read(message.body, charset)
+      .toFlux()
   }
 
   override fun getReadableMediaTypes(): MutableList<MediaType> {
@@ -54,8 +50,8 @@ class AsyncDocumentReader(
   ): Mono<Document> {
     val charset: Charset = message.headers.contentType?.charset ?: Charset.defaultCharset()
 
-    return DataBufferUtils.join(message.body)
-      .flatMap { reader.bufferToDocument(it, charset) }
+    return reader.read(message.body, charset)
+      .toMono()
   }
 
   companion object {
@@ -64,40 +60,3 @@ class AsyncDocumentReader(
   }
 }
 
-@Component
-class AsyncSpecReader {
-  private val parser = Async20Parser()
-
-  init {
-    AMF.init().get()
-  }
-
-  companion object {
-    val ASYNC20_PROFILE: ProfileName = ProfileName.apply("Async20Profile")
-  }
-
-  fun bufferToDocument(buffer: DataBuffer, charset: Charset): Mono<Document> {
-    return Mono.create {
-      try {
-        val msg = buffer.toString(charset)
-
-        val model: Document = parser
-          .parseStringAsync(msg)
-          .get() as Document
-        val v: ValidationReport = AMF.validate(model, ASYNC20_PROFILE) { ASYNC20_PROFILE }
-          .get()
-
-        if (v.conforms()) {
-          it.success(model)
-        } else {
-          val errorMsg = v.results()
-            .map { it.message() }
-            .reduceRight { s, acc -> "$s\\n$acc" }
-          it.error(IllegalArgumentException(errorMsg))
-        }
-      } catch (e: Exception) {
-        it.error(IllegalArgumentException(e.localizedMessage, e))
-      }
-    }
-  }
-}
