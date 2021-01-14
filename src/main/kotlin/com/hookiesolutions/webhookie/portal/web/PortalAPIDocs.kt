ffcc18be55bae80cb9e08db18079ea1bf7fb60c2
Arthur Kazemi <bidadh@gmail.com>
2021-01-14 17:11:55 +1100

    add basic API doc

---
+++ src/main/kotlin/com/hookiesolutions/webhookie/portal/web/PortalAPIDocs.kt
@@ -0,0 +1,24 @@
+package com.hookiesolutions.webhookie.portal.web
+
+import com.hookiesolutions.webhookie.portal.web.AccessGroupController.Companion.REQUEST_MAPPING_PORTAL_ADMIN
+import org.springdoc.core.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/**
+ *
+ * @author Arthur Kazemi<bidadh@gmail.com>
+ * @since 14/1/21 13:53
+ */
+@Configuration
+class PortalAPIDocs {
+  @Bean
+  fun portalOpenApi(): GroupedOpenApi {
+    val paths = arrayOf("${REQUEST_MAPPING_PORTAL_ADMIN}/**")
+    return GroupedOpenApi
+      .builder()
+      .group("Admin Portal")
+      .pathsToMatch(*paths)
+      .build()
+  }
+}
