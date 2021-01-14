5b8d1ec216e3813352f1ccbdc39153f646370827
Arthur Kazemi <bidadh@gmail.com>
2021-01-14 11:03:17 +1100

    minor refactor

--- src/main/kotlin/com/hookiesolutions/webhookie/portal/web/AsyncController.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/portal/web/AsyncController.kt
@@ -1,6 +1,7 @@
 package com.hookiesolutions.webhookie.portal.web
 
 import amf.client.model.document.Document
+import com.hookiesolutions.webhookie.common.Constants.Security.Roles.Companion.ROLE_CONSUMER
 import com.hookiesolutions.webhookie.portal.web.io.AsyncDocumentReader.Companion.TEXT_YAML_VALUE
 import com.hookiesolutions.webhookie.portal.service.model.WebhookGroup
 import com.hookiesolutions.webhookie.portal.service.AsyncApiService
@@ -15,7 +16,7 @@ import reactor.core.publisher.Mono
 class AsyncController(
   private val service: AsyncApiService
 ) {
-  @PreAuthorize("hasAuthority('WH_CONSUMER')")
+  @PreAuthorize("hasAuthority('$ROLE_CONSUMER')")
   @PostMapping("/async/read",
     consumes = [TEXT_YAML_VALUE, APPLICATION_JSON_VALUE],
     produces = [APPLICATION_JSON_VALUE]
