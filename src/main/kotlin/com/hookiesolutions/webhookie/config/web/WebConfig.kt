def82a7b0a60ead517654b7d2b1cda27c6ffc5a0
Arthur Kazemi <bidadh@gmail.com>
2021-01-18 12:16:26 +1100

    EnableWebFlux

---
+++ src/main/kotlin/com/hookiesolutions/webhookie/config/web/WebConfig.kt
@@ -0,0 +1,13 @@
+package com.hookiesolutions.webhookie.config.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.EnableWebFlux
+
+/**
+ *
+ * @author Arthur Kazemi<bidadh@gmail.com>
+ * @since 17/1/21 13:58
+ */
+@Configuration
+@EnableWebFlux
+class WebConfig
