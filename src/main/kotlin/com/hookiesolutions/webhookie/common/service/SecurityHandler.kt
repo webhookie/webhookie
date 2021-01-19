1e5d170660b37b78ef0bfdd082824a001b407f7a
Arthur Kazemi <bidadh@gmail.com>
2021-01-19 19:57:38 +1100

    create SecurityHandler

---
+++ src/main/kotlin/com/hookiesolutions/webhookie/common/service/SecurityHandler.kt
@@ -0,0 +1,28 @@
+package com.hookiesolutions.webhookie.common.service
+
+import com.hookiesolutions.webhookie.config.security.jwt.WebhookieJwtAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+
+/**
+ *
+ * @author Arthur Kazemi<bidadh@gmail.com>
+ * @since 19/1/21 19:36
+ */
+@Component
+class SecurityHandler {
+  fun token(): Mono<WebhookieJwtAuthenticationToken> {
+    return ReactiveSecurityContextHolder.getContext()
+      .map(SecurityContext::getAuthentication)
+      .filter(Authentication::isAuthenticated)
+      .cast(WebhookieJwtAuthenticationToken::class.java)
+  }
+
+  fun groups(): Mono<List<String>> {
+    return token()
+      .map { it.groups }
+  }
+}
