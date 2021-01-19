68ecc46ff29a1537e80e77464e1b3cd20babc1fb
Arthur Kazemi <bidadh@gmail.com>
2021-01-19 14:07:43 +1100

    cleanup

--- src/main/kotlin/com/hookiesolutions/webhookie/config/web/WebConfig.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/config/web/WebConfig.kt
@@ -1,11 +1,7 @@
 package com.hookiesolutions.webhookie.config.web
 
-import com.hookiesolutions.webhookie.portal.web.io.AsyncDocumentEncoder
-import com.hookiesolutions.webhookie.portal.web.io.AsyncDocumentReader
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.web.reactive.config.EnableWebFlux
-import org.springframework.web.reactive.config.WebFluxConfigurer
 
 /**
  *
@@ -14,14 +10,4 @@ import org.springframework.web.reactive.config.WebFluxConfigurer
  */
 @Configuration
 @EnableWebFlux
-class WebConfig(
-  private val reader: AsyncDocumentReader,
-  private val encoder: AsyncDocumentEncoder
-): WebFluxConfigurer {
-  override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
-    configurer.customCodecs().register(reader)
-    configurer.customCodecs().register(encoder)
-
-    super.configureHttpMessageCodecs(configurer)
-  }
-}
+class WebConfig
