1f1f9bb3096d6ec85b348b8bc0663c72704025c9
Arthur Kazemi <bidadh@gmail.com>
2021-01-19 14:07:32 +1100

    fix IRI validation

--- src/main/kotlin/com/hookiesolutions/webhookie/common/validation/IRIValidator.java
+++ src/main/kotlin/com/hookiesolutions/webhookie/common/validation/IRIValidator.java
@@ -1,5 +1,6 @@
 package com.hookiesolutions.webhookie.common.validation;
 
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.validation.ConstraintValidator;
@@ -11,6 +12,7 @@ import java.net.URL;
  * @author Arthur Kazemi<bidadh@gmail.com>
  * @since 17/12/20 23:59
  */
+@Component
 public class IRIValidator implements ConstraintValidator<IRI, String> {
   @Override
   public void initialize(IRI constraintAnnotation) {
