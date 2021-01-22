2896413f98636e41bd296e2d3f9bd5853c01ef0e
Arthur Kazemi <bidadh@gmail.com>
2021-01-22 15:10:05 +1100

    add EntityUpdatedMessage

---
+++ src/main/kotlin/com/hookiesolutions/webhookie/common/message/entity/EntityUpdatedMessage.kt
@@ -0,0 +1,12 @@
+package com.hookiesolutions.webhookie.common.message.entity
+
+/**
+ *
+ * @author Arthur Kazemi<bidadh@gmail.com>
+ * @since 22/1/21 14:50
+ */
+data class EntityUpdatedMessage<T>(
+  override val type: String,
+  val oldValue: T,
+  val newValue: T,
+): EntityModifiedMessage
