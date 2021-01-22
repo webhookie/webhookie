7c31aa7cca64910279ce3498e8d196e47f5c341c
Arthur Kazemi <bidadh@gmail.com>
2021-01-22 14:31:26 +1100

    respond CREATED

--- src/main/kotlin/com/hookiesolutions/webhookie/admin/web/AccessGroupController.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/admin/web/AccessGroupController.kt
@@ -6,6 +6,7 @@ import com.hookiesolutions.webhookie.admin.service.model.SaveGroupRequest
 import com.hookiesolutions.webhookie.admin.web.AdminAPIDocs.Companion.REQUEST_MAPPING_ADMIN
 import com.hookiesolutions.webhookie.common.config.web.OpenAPIConfig.Companion.OAUTH2_SCHEME
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import javax.validation.Valid
@@ -26,6 +28,7 @@ interface AccessGroupController<T: AccessGroup> {
     consumes = [MediaType.APPLICATION_JSON_VALUE],
     produces = [MediaType.APPLICATION_JSON_VALUE]
   )
+  @ResponseStatus(HttpStatus.CREATED)
   fun createGroup(@RequestBody @Valid bodyMono: Mono<SaveGroupRequest>): Mono<T> {
     return serviceDelegator.createAccessGroup(bodyMono)
   }
