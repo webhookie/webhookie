8f6998a406aba07b6c06500b3fecb4027e85e671
Arthur Kazemi <bidadh@gmail.com>
2021-01-29 01:42:46 +1100

    return error if token is empty

--- src/main/kotlin/com/hookiesolutions/webhookie/security/service/SecurityHandler.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/security/service/SecurityHandler.kt
@@ -1,11 +1,14 @@
 package com.hookiesolutions.webhookie.security.service
 
 import com.hookiesolutions.webhookie.security.jwt.WebhookieJwtAuthenticationToken
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.switchIfEmpty
+import reactor.kotlin.core.publisher.toMono
 
 /**
  *
@@ -23,11 +26,13 @@ class SecurityHandler {
 
   fun groups(): Mono<List<String>> {
     return token()
+      .switchIfEmpty { AccessDeniedException("Token is not provided").toMono() }
       .map { it.groups }
   }
 
   fun entity(): Mono<String> {
     return token()
+      .switchIfEmpty { AccessDeniedException("Token is not provided").toMono() }
       .map { it.entity }
   }
 }
