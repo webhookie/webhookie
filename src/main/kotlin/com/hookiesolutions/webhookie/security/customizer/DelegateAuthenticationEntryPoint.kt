6a731e705eef7e5abb8e361e9b7484fd639f9c83
Arthur Kazemi <bidadh@gmail.com>
2021-02-01 14:17:19 +1100

    minor cleanup

--- src/main/kotlin/com/hookiesolutions/webhookie/security/customizer/DelegateAuthenticationEntryPoint.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/security/customizer/DelegateAuthenticationEntryPoint.kt
@@ -3,14 +3,11 @@ package com.hookiesolutions.webhookie.security.customizer
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.oauth2.server.resource.web.server.BearerTokenServerAuthenticationEntryPoint
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
 
 class DelegateAuthenticationEntryPoint : ServerAuthenticationEntryPoint {
   private val bearerEntryPoint = BearerTokenServerAuthenticationEntryPoint()
-  @Suppress("unused")
-  private val redirectServerAuthenticationEntryPoint = RedirectServerAuthenticationEntryPoint("http://www.yahoo.com")
 
   override fun commence(exchange: ServerWebExchange, ex: AuthenticationException): Mono<Void> {
     return bearerEntryPoint.commence(exchange, ex)
