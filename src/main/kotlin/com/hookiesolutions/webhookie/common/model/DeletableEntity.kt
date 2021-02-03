06950c64c30664976539f45997d539570ac0f1fe
Arthur Kazemi <bidadh@gmail.com>
2021-02-03 11:51:11 +1100

    refactor

--- src/main/kotlin/com/hookiesolutions/webhookie/common/model/DeletableEntity.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/common/model/DeletableEntity.kt
@@ -3,4 +3,10 @@ package com.hookiesolutions.webhookie.common.model
 data class DeletableEntity<T: AbstractEntity>(
   val entity: T,
   val deletable: Boolean
-)
+) {
+  companion object {
+    fun <T: AbstractEntity> deletable(entity: T): DeletableEntity<T> {
+      return deletable(entity)
+    }
+  }
+}
--- src/main/kotlin/com/hookiesolutions/webhookie/common/model/UpdatableEntity.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/common/model/UpdatableEntity.kt
@@ -3,4 +3,11 @@ package com.hookiesolutions.webhookie.common.model
 data class UpdatableEntity<T: AbstractEntity>(
   val entity: T,
   val updatable: Boolean
-)
+) {
+  companion object {
+    @JvmStatic
+    fun <T: AbstractEntity> updatable(entity: T): UpdatableEntity<T> {
+      return UpdatableEntity(entity, true)
+    }
+  }
+}
