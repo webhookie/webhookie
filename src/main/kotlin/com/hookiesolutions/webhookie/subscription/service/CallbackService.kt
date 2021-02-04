e5a0bdb5afa1b060c033b59aabe91912ebe466a1
Arthur Kazemi <bidadh@gmail.com>
2021-02-04 11:28:29 +1100

    add VerifyApplicationReadAccessById and VerifyApplicationWriteAccessById

--- src/main/kotlin/com/hookiesolutions/webhookie/subscription/service/CallbackService.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/subscription/service/CallbackService.kt
@@ -3,13 +3,12 @@ package com.hookiesolutions.webhookie.subscription.service
 import com.hookiesolutions.webhookie.common.Constants.Security.Roles.Companion.ROLE_CONSUMER
 import com.hookiesolutions.webhookie.common.model.DeletableEntity.Companion.deletable
 import com.hookiesolutions.webhookie.common.model.UpdatableEntity.Companion.updatable
-import com.hookiesolutions.webhookie.subscription.domain.Application
-import com.hookiesolutions.webhookie.subscription.domain.ApplicationRepository
 import com.hookiesolutions.webhookie.subscription.domain.Callback
 import com.hookiesolutions.webhookie.subscription.domain.CallbackRepository
 import com.hookiesolutions.webhookie.subscription.service.model.CallbackRequest
+import com.hookiesolutions.webhookie.subscription.service.security.annotation.VerifyApplicationReadAccessById
+import com.hookiesolutions.webhookie.subscription.service.security.annotation.VerifyApplicationWriteAccessById
 import org.slf4j.Logger
-import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
@@ -23,62 +22,47 @@ import reactor.core.publisher.Mono
 @Service
 class CallbackService(
   private val repository: CallbackRepository,
-  private val applicationRepository: ApplicationRepository,
   private val log: Logger
 ) {
   @PreAuthorize("hasAuthority('${ROLE_CONSUMER}')")
+  @VerifyApplicationWriteAccessById
   fun createCallback(applicationId: String, request: CallbackRequest): Mono<Callback> {
     log.info("adding Callback: '{}' to application: '{}'", request.requestTarget(), applicationId)
-    return fetchApplicationWithWriteAccess(applicationId)
-      .map { request.callback(applicationId) }
-      .flatMap { repository.save(it)}
+    val callback = request.callback(applicationId)
+    return repository.save(callback)
       .doOnNext { log.info("Callback '{}' was created successfully", it.details.requestTarget()) }
   }
 
   @PreAuthorize("hasAuthority('${ROLE_CONSUMER}')")
+  @VerifyApplicationReadAccessById
   fun applicationCallbacks(applicationId: String): Flux<Callback> {
     log.info("fetching all application callbacks : '{}'", applicationId)
-    return fetchApplicationWithReadAccess(applicationId)
-      .flatMapMany { repository.findApplicationCallbacks(applicationId) }
+    return repository.findApplicationCallbacks(applicationId)
   }
 
   @PreAuthorize("hasAuthority('${ROLE_CONSUMER}')")
+  @VerifyApplicationReadAccessById
   fun applicationCallbackById(applicationId: String, id: String): Mono<Callback> {
     log.info("fetching callback : '{}'", id)
-    return fetchApplicationWithReadAccess(applicationId)
-      .flatMap { repository.findById(id) }
+    return repository.findById(id)
   }
 
   @PreAuthorize("hasAuthority('${ROLE_CONSUMER}')")
+  @VerifyApplicationWriteAccessById
   fun deleteApplicationCallbackById(applicationId: String, id: String): Mono<String> {
     log.info("deleting callback : '{}'", id)
-    return fetchApplicationWithWriteAccess(applicationId)
-      .flatMap { repository.findById(id) }
+    return repository.findById(id)
       .map { deletable(it) }
       .flatMap { repository.delete(it) }
   }
 
   @PreAuthorize("hasAuthority('${ROLE_CONSUMER}')")
+  @VerifyApplicationWriteAccessById
   fun updateCallback(applicationId: String, id: String, body: CallbackRequest): Mono<Callback> {
     log.info("updating Callback: '{}', '{}'", body.requestTarget(), id)
-    return fetchApplicationWithWriteAccess(applicationId)
-      .flatMap { repository.findById(id) }
+    return repository.findById(id)
       .map { body.copy(it, applicationId) }
       .map { updatable(it) }
       .flatMap { repository.update(it, id) }
-  }
-
-  private fun fetchApplicationWithReadAccess(applicationId: String): Mono<Application> {
-    return applicationRepository.findByIdVerifyingReadAccess(applicationId)
-      .doOnError(AccessDeniedException::class.java) {
-        log.warn("Current user doesn't have access to the application: '{}'", applicationId)
-      }
-  }
-
-  private fun fetchApplicationWithWriteAccess(applicationId: String): Mono<Application> {
-    return applicationRepository.findByIdVerifyingWriteAccess(applicationId)
-      .doOnError(AccessDeniedException::class.java) {
-        log.warn("Current user doesn't have access to the application: '{}'", applicationId)
-      }
   }
 }
--- src/main/kotlin/com/hookiesolutions/webhookie/subscription/service/security/aspect/ApplicationSecurityAspect.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/subscription/service/security/aspect/ApplicationSecurityAspect.kt
@@ -1,14 +1,22 @@
 package com.hookiesolutions.webhookie.subscription.service.security.aspect
 
 import com.hookiesolutions.webhookie.subscription.domain.Application
+import com.hookiesolutions.webhookie.subscription.domain.ApplicationRepository
+import com.hookiesolutions.webhookie.subscription.domain.Callback
 import com.hookiesolutions.webhookie.subscription.service.security.ApplicationSecurityService
+import com.hookiesolutions.webhookie.subscription.service.security.annotation.VerifyApplicationReadAccessById
+import com.hookiesolutions.webhookie.subscription.service.security.annotation.VerifyApplicationWriteAccessById
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
+import org.aspectj.lang.reflect.MethodSignature
 import org.slf4j.Logger
 import org.springframework.stereotype.Component
+import reactor.core.CorePublisher
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.lang.reflect.Method
 
 /**
  *
@@ -19,10 +27,19 @@ import reactor.core.publisher.Mono
 @Component
 class ApplicationSecurityAspect(
   private val securityService: ApplicationSecurityService,
-  private val log: Logger
+  private val applicationRepository: ApplicationRepository,
+  private val log: Logger,
 ) {
   @Pointcut("@annotation(com.hookiesolutions.webhookie.subscription.service.security.annotation.VerifyApplicationReadAccess)")
   fun annotatedVerifyApplicationReadAccess() {
+  }
+
+  @Pointcut("@annotation(com.hookiesolutions.webhookie.subscription.service.security.annotation.VerifyApplicationReadAccessById)")
+  fun annotatedVerifyApplicationReadAccessById() {
+  }
+
+  @Pointcut("@annotation(com.hookiesolutions.webhookie.subscription.service.security.annotation.VerifyApplicationWriteAccessById)")
+  fun annotatedVerifyApplicationWriteAccessById() {
   }
 
   @Pointcut("@annotation(com.hookiesolutions.webhookie.subscription.service.security.annotation.VerifyApplicationWriteAccess)")
@@ -45,6 +62,45 @@ class ApplicationSecurityAspect(
             log.debug("Verifying Application '{}' Read Access...", it.name)
           }
         }
+    }
+  }
+
+  @Around("annotatedVerifyApplicationReadAccessById()")
+  fun checkReadAccessById(pjp: ProceedingJoinPoint): Any {
+    val method: Method = extractMethod(pjp)
+
+    val ann = method.getAnnotation(VerifyApplicationReadAccessById::class.java)
+
+    val id = pjp.args[ann.idPosition] as String
+    val applicationMono = applicationRepository.findByIdVerifyingReadAccess(id)
+    return applicationData(pjp, applicationMono)
+  }
+
+  @Around("annotatedVerifyApplicationWriteAccessById()")
+  fun checkWriteAccessById(pjp: ProceedingJoinPoint): Any {
+    val method: Method = extractMethod(pjp)
+
+    val ann = method.getAnnotation(VerifyApplicationWriteAccessById::class.java)
+
+    val id = pjp.args[ann.idPosition] as String
+    val applicationMono = applicationRepository.findByIdVerifyingWriteAccess(id)
+    return applicationData(pjp, applicationMono)
+  }
+
+  private fun extractMethod(pjp: ProceedingJoinPoint): Method {
+    return (pjp.signature as MethodSignature).method
+  }
+
+  private fun applicationData(pjp: ProceedingJoinPoint, applicationMono: Mono<Application>): CorePublisher<Callback> {
+    val returnType: Class<*> = extractMethod(pjp).returnType
+    return if(Mono::class.java.isAssignableFrom(returnType)) {
+      @Suppress("UNCHECKED_CAST")
+      val mono = pjp.proceed() as Mono<Callback>
+      applicationMono.flatMap { mono }
+    } else {
+      @Suppress("UNCHECKED_CAST")
+      val flux = pjp.proceed() as Flux<Callback>
+      applicationMono.flatMapMany { flux }
     }
   }
 
