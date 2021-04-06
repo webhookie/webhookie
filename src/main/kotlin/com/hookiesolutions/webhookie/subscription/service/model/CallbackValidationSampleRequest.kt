9ae48dd1fd1ef713d5b6b7ad98bfa35de50b9498
Arthur Kazemi <bidadh@gmail.com>
2021-04-06 11:40:20 +1000

    add key id to callback test requests

--- src/main/kotlin/com/hookiesolutions/webhookie/subscription/service/model/CallbackValidationSampleRequest.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/subscription/service/model/CallbackValidationSampleRequest.kt
@@ -2,6 +2,7 @@ package com.hookiesolutions.webhookie.subscription.service.model
 
 import com.hookiesolutions.webhookie.common.message.subscription.SubscriptionSignature
 import com.hookiesolutions.webhookie.subscription.domain.CallbackDetails
+import com.hookiesolutions.webhookie.subscription.domain.Secret
 import com.hookiesolutions.webhookie.subscription.utils.CryptoUtils
 import org.bson.types.ObjectId
 import org.springframework.http.HttpHeaders
@@ -14,7 +15,7 @@ data class CallbackValidationSampleRequest(
   val url: String,
   val payload: String,
   val headers: HttpHeaders,
-  val secret: String? = null,
+  val secret: Secret? = null,
   val traceId: String? = null,
   val spanId: String? = null
 ) {
@@ -28,16 +29,17 @@ data class CallbackValidationSampleRequest(
   ) {
     val traceId = this.traceId?: ObjectId.get().toHexString()
     val spanId = this.spanId?: ObjectId.get().toHexString()
-    Mono.create<String> { it.success(secret) }
-      .flatMap { CryptoUtils.hmac(it, callback, time.toString(), traceId, spanId) }
+    Mono
+      .create<Secret> { it.success(secret) }
+      .zipWhen { CryptoUtils.hmac(it.secret, callback, time.toString(), traceId, spanId) }
       .map {
         SubscriptionSignature.Builder()
-          .keyId("keyId")
+          .keyId(it.t1.keyId)
           .algorithm(CryptoUtils.ALG)
           .traceId(traceId)
           .spanId(spanId)
           .date(time)
-          .signature(it)
+          .signature(it.t2)
           .build()
       }
       .subscribe { signature ->
@@ -64,7 +66,7 @@ data class CallbackValidationSampleRequest(
       callbackDetails.url,
       payload,
       headers,
-      callbackDetails.security?.secret?.secret
+      callbackDetails.security?.secret
     )
   }
 }
