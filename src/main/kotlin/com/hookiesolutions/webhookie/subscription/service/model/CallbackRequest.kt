ea7e61cdb6c0525c6c163a73c8157de36d6d4f7c
Arthur Kazemi <bidadh@gmail.com>
2021-07-02 01:23:57 +1000

    handle unmodified secret in update callback

--- src/main/kotlin/com/hookiesolutions/webhookie/subscription/service/model/CallbackRequest.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/subscription/service/model/CallbackRequest.kt
@@ -3,6 +3,7 @@ package com.hookiesolutions.webhookie.subscription.service.model
 import com.hookiesolutions.webhookie.common.validation.Url
 import com.hookiesolutions.webhookie.subscription.domain.Callback
 import com.hookiesolutions.webhookie.subscription.domain.CallbackSecurity
+import com.hookiesolutions.webhookie.subscription.domain.Secret
 import org.springframework.http.HttpMethod
 import javax.validation.constraints.NotBlank
 
@@ -35,11 +36,28 @@ data class CallbackRequest(
 
   //TODO: refactor this and use mongodb update instead
   fun copy(entity: Callback, applicationId: String): Callback {
-    val result = Callback(name, applicationId, httpMethod, url, security)
+    val result = Callback(name, applicationId, httpMethod, url, createCallbackSecurity(entity.security, security))
     result.version = entity.version
     result.id = entity.id
     result.createdDate = entity.createdDate
     result.createdBy = entity.createdBy
     return result
+  }
+
+  private fun createCallbackSecurity(currentSecurity: CallbackSecurity?, newSecurity: CallbackSecurity?): CallbackSecurity? {
+    if(newSecurity == null) {
+      return null
+    }
+
+    val secret = newSecurity.secret.secret
+    if(currentSecurity == null) {
+      return CallbackSecurity(secret = Secret(newSecurity.secret.keyId, secret))
+    }
+
+    return if(secret.replace("*", "").trim().isNotBlank()) {
+      CallbackSecurity(secret = Secret(newSecurity.secret.keyId, secret))
+    } else {
+      CallbackSecurity(secret = Secret(newSecurity.secret.keyId, currentSecurity.secret.secret))
+    }
   }
 }
