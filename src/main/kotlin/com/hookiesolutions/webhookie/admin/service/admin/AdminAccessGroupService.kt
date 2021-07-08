40c74d8eb0691a7e30bad5bb22ae9e6ffb82d81a
Arthur Kazemi <bidadh@gmail.com>
2021-07-08 14:52:19 +1000

    refactor and cleanup

--- src/main/kotlin/com/hookiesolutions/webhookie/admin/service/admin/AdminAccessGroupService.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/admin/service/admin/AdminAccessGroupService.kt
@@ -1,15 +1,10 @@
 package com.hookiesolutions.webhookie.admin.service.admin
 
 import com.hookiesolutions.webhookie.admin.domain.AccessGroup
-import com.hookiesolutions.webhookie.admin.domain.AccessGroupRepository
-import com.hookiesolutions.webhookie.admin.service.AccessGroupFactory
 import com.hookiesolutions.webhookie.admin.service.AccessGroupService
-import com.hookiesolutions.webhookie.admin.service.EntityEventPublisher
 import com.hookiesolutions.webhookie.admin.service.model.SaveGroupRequest
 import com.hookiesolutions.webhookie.common.Constants
 import com.hookiesolutions.webhookie.common.annotation.Open
-import com.hookiesolutions.webhookie.security.service.SecurityHandler
-import org.slf4j.Logger
 import org.springframework.security.access.prepost.PreAuthorize
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -21,40 +16,35 @@ import reactor.core.publisher.Mono
  */
 @Open
 class AdminAccessGroupService<T : AccessGroup>(
-  override val repository: AccessGroupRepository<T>,
-  override val factory: AccessGroupFactory,
-  override val publisher: EntityEventPublisher,
-  override val securityHandler: SecurityHandler,
-  override val log: Logger,
-  override val clazz: Class<T>,
-): AccessGroupService<T>(repository, factory, publisher, securityHandler, log, clazz) {
+  val service: AccessGroupService<T>,
+) {
   @PreAuthorize("hasAuthority('${Constants.Security.Roles.ROLE_ADMIN}')")
-  override fun createGroup(body: SaveGroupRequest): Mono<T> {
-    return super.createGroup(body)
+  fun createGroup(body: SaveGroupRequest): Mono<T> {
+    return service.createGroup(body)
   }
 
   @PreAuthorize("isAuthenticated()")
-  override fun allGroups(): Flux<T> {
-    return super.allGroups()
+  fun allGroups(): Flux<T> {
+    return service.allGroups()
   }
 
   @PreAuthorize("hasAuthority('${Constants.Security.Roles.ROLE_ADMIN}')")
-  override fun groupById(id: String): Mono<T> {
-    return super.groupById(id)
+  fun groupById(id: String): Mono<T> {
+    return service.groupById(id)
   }
 
   @PreAuthorize("hasAuthority('${Constants.Security.Roles.ROLE_ADMIN}')")
-  override fun groupByIAM(iamGroupName: String): Mono<T> {
-    return super.groupByIAM(iamGroupName)
+  fun groupByIAM(iamGroupName: String): Mono<T> {
+    return service.groupByIAM(iamGroupName)
   }
 
   @PreAuthorize("hasAuthority('${Constants.Security.Roles.ROLE_ADMIN}')")
-  override fun deleteGroupById(id: String): Mono<String> {
-    return super.deleteGroupById(id)
+  fun deleteGroupById(id: String): Mono<String> {
+    return service.deleteGroupById(id)
   }
 
   @PreAuthorize("hasAuthority('${Constants.Security.Roles.ROLE_ADMIN}')")
-  override fun updateGroupById(id: String, body: SaveGroupRequest): Mono<T> {
-    return super.updateGroupById(id, body)
+  fun updateGroupById(id: String, body: SaveGroupRequest): Mono<T> {
+    return service.updateGroupById(id, body)
   }
 }
--- src/main/kotlin/com/hookiesolutions/webhookie/admin/service/admin/AdminProviderGroupService.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/admin/service/admin/AdminProviderGroupService.kt
@@ -1,11 +1,7 @@
 package com.hookiesolutions.webhookie.admin.service.admin
 
-import com.hookiesolutions.webhookie.admin.domain.AccessGroupRepository
 import com.hookiesolutions.webhookie.admin.domain.ProviderGroup
-import com.hookiesolutions.webhookie.admin.service.AccessGroupFactory
-import com.hookiesolutions.webhookie.admin.service.EntityEventPublisher
-import com.hookiesolutions.webhookie.security.service.SecurityHandler
-import org.slf4j.Logger
+import com.hookiesolutions.webhookie.admin.service.ProviderGroupService
 import org.springframework.stereotype.Service
 
 /**
@@ -15,11 +11,5 @@ import org.springframework.stereotype.Service
  */
 @Service
 class AdminProviderGroupService(
-  override val repository: AccessGroupRepository<ProviderGroup>,
-  override val factory: AccessGroupFactory,
-  override val publisher: EntityEventPublisher,
-  override val securityHandler: SecurityHandler,
-  override val log: Logger,
-): AdminAccessGroupService<ProviderGroup>(
-  repository, factory, publisher, securityHandler, log, ProviderGroup::class.java
-)
+  override val service: ProviderGroupService,
+): AdminAccessGroupService<ProviderGroup>(service)
