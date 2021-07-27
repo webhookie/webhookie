3a80785dc5858cfda0d9993e9674d0b596daa7f4
Arthur Kazemi <bidadh@gmail.com>
2021-07-27 17:50:40 +1000

    Add copyright

--- src/main/kotlin/com/hookiesolutions/webhookie/audit/domain/TraceRepository.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/audit/domain/TraceRepository.kt
@@ -1,3 +1,25 @@
+/*
+ * webhookie - webhook infrastructure that can be incorporated into any microservice or integration architecture.
+ * Copyright (C) 2021 Hookie Solutions AB, info@hookiesolutions.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * If your software can interact with users remotely through a computer network, you should also make sure that it provides a way for users to get its source. For example, if your program is a web application, its interface could display a "Source" link that leads users to an archive of the code. There are many ways you could offer source, and different solutions will be better for different programs; see section 13 for the specific requirements.
+ *
+ * You should also get your employer (if you work as a programmer) or school, if any, to sign a "copyright disclaimer" for the program, if necessary. For more information on this, and how to apply and follow the GNU AGPL, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.hookiesolutions.webhookie.audit.domain
 
 import com.hookiesolutions.webhookie.audit.domain.Span.Queries.Companion.spanTopicIn
@@ -8,13 +30,12 @@ import com.hookiesolutions.webhookie.audit.domain.Trace.Keys.Companion.KEY_TIME
 import com.hookiesolutions.webhookie.audit.domain.Trace.Queries.Companion.byTraceId
 import com.hookiesolutions.webhookie.audit.domain.Trace.Queries.Companion.statusIn
 import com.hookiesolutions.webhookie.audit.domain.Trace.Queries.Companion.traceIdRegex
-import com.hookiesolutions.webhookie.audit.domain.Trace.Queries.Companion.traceStatusIsNot
 import com.hookiesolutions.webhookie.audit.domain.Trace.Queries.Companion.traceTopicIn
 import com.hookiesolutions.webhookie.audit.domain.Trace.Queries.Companion.traceTopicIs
 import com.hookiesolutions.webhookie.audit.domain.Trace.Queries.Companion.traceUpdatedAfter
 import com.hookiesolutions.webhookie.audit.domain.Trace.Queries.Companion.traceUpdatedBefore
-import com.hookiesolutions.webhookie.audit.domain.Trace.Updates.Companion.traceStatusUpdate
 import com.hookiesolutions.webhookie.audit.domain.Trace.Updates.Companion.updateSummary
+import com.hookiesolutions.webhookie.audit.domain.TraceStatusUpdate.Keys.Companion.KEY_STATUS
 import com.hookiesolutions.webhookie.audit.domain.TraceSummary.Keys.Companion.KEY_NUMBER_OF_SPANS
 import com.hookiesolutions.webhookie.audit.domain.TraceSummary.Keys.Companion.KEY_NUMBER_OF_SUCCESS
 import com.hookiesolutions.webhookie.audit.domain.aggregation.TraceAggregationStrategy
@@ -23,6 +44,7 @@ import com.hookiesolutions.webhookie.audit.web.model.request.TraceRequest
 import com.hookiesolutions.webhookie.common.model.AbstractEntity.Queries.Companion.filters
 import com.hookiesolutions.webhookie.common.model.FieldMatchingStrategy
 import com.hookiesolutions.webhookie.common.repository.GenericRepository
+import org.bson.Document
 import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.domain.PageRequest
@@ -32,6 +54,7 @@ import org.springframework.data.mongodb.core.FindAndModifyOptions
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.aggregation.AddFieldsOperation
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators
 import org.springframework.data.mongodb.core.aggregation.ArrayOperators
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query.query
@@ -63,6 +86,63 @@ class TraceRepository(
   }
 
   fun addStatus(traceId: String, statusUpdate: TraceStatusUpdate): Mono<Trace> {
+    val historyField = mongoField(KEY_STATUS_HISTORY)
+    val subtract = ArithmeticOperators.Subtract
+      .valueOf(ArrayOperators.arrayOf(historyField).length())
+      .subtract(1)
+/*
+    val slice = ArrayOperators.arrayOf(listOf(historyField, 0, s)).slice()
+    val slice = AggregationOperation {
+      it.getMappedObject(Document.parse("""
+        aa: { {${'$'}slice : ["${'$'}statusHistory        ", 0, "        ${'$'}aSize        "]}
+      """.trimIndent()))
+    }
+*/
+
+    val value = conditionalValue(
+      neExpression("$KEY_STATUS_UPDATE.$KEY_STATUS", "update.status"),
+      ArrayOperators.ConcatArrays.arrayOf(mongoField(KEY_STATUS_HISTORY)).concat(mongoField("uA")),
+      concatArrays("aa", "uA")
+    )
+
+    mongoSet(KEY_STATUS_HISTORY, value)
+    mongoSetLastElemOfArray(KEY_STATUS_HISTORY, KEY_STATUS_UPDATE)
+
+
+    val operations = arrayOf(
+      AddFieldsOperation
+        .addField("aSize")
+        .withValue(subtract)
+        .build(),
+      AggregationOperation {
+        Document.parse("""
+          {
+            ${'$'}addFields: {
+              "aa": {
+                ${'$'}cond: [
+                  {${'$'}eq: [{"${'$'}size": "${'$'}statusHistory"}, 1]},
+                  "${'$'}statusHistory",
+                  {${'$'}slice: ["${'$'}statusHistory", 0, "${'$'}aSize"]}
+                ]
+              }
+            }
+          }
+        """.trimIndent())
+      },
+      AddFieldsOperation
+        .addField("update")
+        .withValueOf(statusUpdate)
+        .build(),
+      addMongoObjectToArrayField("uA", statusUpdate),
+      mongoSet(KEY_STATUS_HISTORY, value),
+      mongoSetLastElemOfArray(KEY_STATUS_HISTORY, KEY_STATUS_UPDATE),
+      mongoUnset("aSize", "aa", "uA", "update")
+    )
+
+    return aggregationUpdate(byTraceId(traceId), Trace::class.java, *operations)
+
+/*
+
     return mongoTemplate
       .findAndModify(
         query(byTraceId(traceId).andOperator(traceStatusIsNot(statusUpdate.status))),
@@ -70,6 +150,7 @@ class TraceRepository(
         FindAndModifyOptions.options().returnNew(true),
         Trace::class.java
       )
+*/
   }
 
   fun updateWithSummary(traceId: String, summary: TraceSummary, traceStatusUpdate: TraceStatusUpdate): Mono<Trace> {
