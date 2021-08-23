d8455398bec252117e416e728a7ff083a5ed96d2
Arthur Kazemi <bidadh@gmail.com>
2021-08-23 17:42:08 +1000

    upgrade spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-	id("org.springframework.boot") version "2.5.3"
+	id("org.springframework.boot") version "2.5.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.0"
-	kotlin("plugin.spring") version "1.5.0"
-	kotlin("plugin.allopen") version "1.5.0"
-	kotlin("kapt") version "1.4.21"
+	kotlin("jvm") version "1.5.21"
+	kotlin("plugin.spring") version "1.5.21"
+	kotlin("plugin.allopen") version "1.5.21"
+	kotlin("kapt") version "1.5.21"
 }
 
 kapt.includeCompileClasspath = false
