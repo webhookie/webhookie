490e8fc327616eee48a694416e0be59d5fa4a7e4
Arthur Kazemi <bidadh@gmail.com>
2021-11-28 07:31:38 +0330

    get token for sample requests

--- http-requests/response-handlers/token.js
+++ http-requests/response-handlers/token.js
@@ -1,6 +1,6 @@
 client.test("Request executed successfully", function () {
     client.assert(response.status === 200, "Response status is not 200");
     client.assert(response.body.hasOwnProperty("access_token"), "Cannot find 'access_token' option in response");
-    client.global.set("token", response.body.id_token);
+    client.global.set("token", response.body.access_token);
     client.global.set("refresh_token", response.body.refresh_token);
 });
