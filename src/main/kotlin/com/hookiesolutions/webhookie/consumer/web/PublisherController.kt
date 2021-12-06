a3b7724577d5f334d9e5a502dbc90f1a5463aab6
Arthur Kazemi <bidadh@gmail.com>
2021-12-07 00:24:15 +1100

    minor cleanup

--- src/main/kotlin/com/hookiesolutions/webhookie/consumer/web/PublisherController.kt
+++ src/main/kotlin/com/hookiesolutions/webhookie/consumer/web/PublisherController.kt
@@ -26,7 +26,6 @@ import com.hookiesolutions.webhookie.common.Constants.Queue.Headers.Companion.HE
 import com.hookiesolutions.webhookie.common.Constants.Queue.Headers.Companion.WH_HEADER_AUTHORIZED_SUBSCRIBER
 import com.hookiesolutions.webhookie.common.Constants.Queue.Headers.Companion.WH_HEADER_TOPIC
 import com.hookiesolutions.webhookie.common.Constants.Queue.Headers.Companion.WH_HEADER_TRACE_ID
-import com.hookiesolutions.webhookie.common.Constants.Queue.Headers.Companion.WH_HEADER_TRACE_ID_MISSING
 import com.hookiesolutions.webhookie.common.config.web.OpenAPIConfig.Companion.OAUTH2_SCHEME
 import com.hookiesolutions.webhookie.consumer.service.TrafficServiceDelegate
 import com.hookiesolutions.webhookie.consumer.web.ConsumerAPIDocs.Companion.REQUEST_MAPPING_CONSUMER
@@ -34,16 +33,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.slf4j.Logger
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import org.springframework.messaging.Message
 import org.springframework.messaging.SubscribableChannel
 import org.springframework.messaging.support.MessageBuilder
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 
 /**
  *
@@ -89,34 +82,6 @@ class PublisherController(
       }
       .doOnNext { log.debug("Message with traceId: '{}' is being processed") }
       .map { "OK" }
-  }
-
-  @PostMapping("/event2", produces = [MediaType.TEXT_PLAIN_VALUE])
-  fun publishEvent2(
-    @RequestBody body: Message<ByteArray>,
-    @RequestHeader(WH_HEADER_TRACE_ID, required = false, defaultValue = "") traceId: String,
-  ): Mono<String> {
-    val responseMono = if (body.headers[WH_HEADER_TRACE_ID_MISSING] == "true") {
-      sendMessage(body)
-    } else {
-      traceServiceDelegate.checkOrGenerateTrace(traceId)
-        .doOnError { log.warn("Message was rejected due to duplicate traceId '{}'", traceId) }
-        .flatMap { sendMessage(body) }
-    }
-
-    return responseMono
-      .doOnNext { log.debug("Message with traceId: '{}' is being processed") }
-      .map { "OK" }
-  }
-
-  private fun sendMessage(body: Message<ByteArray>): Mono<Boolean> {
-    log.info("Publishing a message to event queue....")
-    val msg = MessageBuilder
-      .withPayload(body.payload)
-      .copyHeaders(body.headers)
-      .build()
-    return internalConsumerChannel.send(msg)
-      .toMono()
   }
 
   companion object {
